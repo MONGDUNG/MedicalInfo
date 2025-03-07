685ccd6e5ae138c402c93add0611dda440e758f4
MONGDUNG <14A@192.168.219.83>
2025-03-07 16:56:03 +0900

    commit

--- src/main/java/com/global/map/service/MapService.java
+++ src/main/java/com/global/map/service/MapService.java
@@ -85,6 +85,10 @@ public class MapService {
 
         return result;
     }
+    
+    
+    
+    
     private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
         double R = 6371 * 1000; // 지구 반지름 (m)
         double dLat = Math.toRadians(lat2 - lat1);
@@ -133,95 +137,14 @@ public class MapService {
                 .build();
     }
 
-
-	        return emergencys.stream()
-	                .map(entity -> emergencyToDTO(entity, calculateDistance(
-	                        centerLat, centerLng,
-	                        Double.parseDouble(entity.getLatitude()),
-	                        Double.parseDouble(entity.getLongitude())
-	                )))
-	                .limit(1000)  // 최대 갯수
-	                .toList();
-	    }
-	    
-	    //거리 계산
-	    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-	        double R = 6371 * 1000; // 지구 반지름 (m)
-	        double dLat = Math.toRadians(lat2 - lat1);
-	        double dLon = Math.toRadians(lon2 - lon1);
-	        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-	                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-	                 * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-	        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	        return R * c;
-	    }
-	
-	
-	private MedinstDTO medinstToDTO(MedinstEntity entity, double distance) {
-	    return MedinstDTO.builder()
-	            .id(entity.getId())
-	            .hospitalCode(entity.getHospitalCode())
-	            .hospitalName(entity.getHospitalName())
-	            .categoryCode(entity.getCategoryCode())
-	            .postalCode(entity.getPostalCode())
-	            .address(entity.getAddress())
-	            .phoneNumber(entity.getPhoneNumber())
-	            .website(entity.getWebsite())
-	            .establishedDate(entity.getEstablishedDate() != null ? entity.getEstablishedDate().toString() : null)
-	            .totalDoc(entity.getTotalDoc())
-	            .latitude(Double.parseDouble(entity.getLatitude()))
-	            .longitude(Double.parseDouble(entity.getLongitude()))
-	            .distance(distance)  // 계산된 거리 추가
-	            .build();
-	}
-	
-	private PharmacyDTO pharmacyToDTO(PharmacyEntity entity, double distance) {
-	    return PharmacyDTO.builder()
-	            .id(entity.getId())
-	            .pharmacyName(entity.getPharmacyName())
-	            .address(entity.getAddress())
-	            .phoneNumber(entity.getPhoneNumber())
-	            .postalCode(entity.getPostalcode())
-	            .latitude(Double.parseDouble(entity.getLatitude()))
-	            .longitude(Double.parseDouble(entity.getLongitude()))	            
-	            .establishedDate(entity.getEstablishedDate() != null ? entity.getEstablishedDate().toString() : null)
-	            .distance(distance)  // 계산된 거리 추가
-	            .build();
-	}
-	
-	private ConvenienceStoreDTO convenienceStoreToDTO(ConvenienceStoreEntity entity, double distance) {
-	    return ConvenienceStoreDTO.builder()
-	            .id(entity.getId())
-	            .storeCode(entity.getStoreCode())
-	            .licenseDate(entity.getLicenseDate()!= null ? entity.getLicenseDate().toString() : null)
-	            .phoneNumber(entity.getPhoneNumber())
-	            .address(entity.getAddress())
-	            .postalCode(entity.getPostalCode())
-	            .storeName(entity.getStoreName())
-	            .latitude(Double.parseDouble(entity.getLatitude()))
-	            .longitude(Double.parseDouble(entity.getLongitude()))
-	            .distance(distance)  // 계산된 거리 추가
-	            .build();
-	}   
-	
-	private EmergencyDTO emergencyToDTO(EmergencyEntity entity, double distance) {
-	    return EmergencyDTO.builder()
-	            .erId(entity.getErId())
-	            .hospitalName(entity.getHospitalName())
-	            .phoneNumber(entity.getPhoneNumber())
-	            .address(entity.getAddress())
-	            .mon(entity.getMon())
-	            .tues(entity.getTues())
-	            .wednes(entity.getWednes())
-	            .thurs(entity.getThurs())
-	            .fri(entity.getFri())
-	            .satur(entity.getSatur())
-	            .sun(entity.getSun())
-	            .holi(entity.getHoli())
-	            .latitude(Double.parseDouble(entity.getLatitude()))
-	            .longitude(Double.parseDouble(entity.getLongitude()))
-	            .build();
-	}
-	//push 확인
+    private ConvenienceStoreDTO storeToDTO(ConvenienceStoreEntity entity, double distance) {
+        return ConvenienceStoreDTO.builder()
+                .id(entity.getId())
+                .storeName(entity.getStoreName())
+                .address(entity.getAddress())
+                .latitude(Double.parseDouble(entity.getLatitude()))
+                .longitude(Double.parseDouble(entity.getLongitude()))
+                .distance(distance)
+                .build();
+    }
 }
-
